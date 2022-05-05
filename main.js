965ff5c43e9e2e878b5e3f1a425c9e13270339dc
Gabriella Oliveira <gaby999@outlook.com>
2022-05-05 20:46:39 -0300

    stage3e4

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
 function onScroll() {
+    showNavOnScroll()
+    showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll(){
     if(scrollY > 0){
         navigation.classList.add('scroll')
     } else {
         navigation.classList.remove('scroll')
     }
-    
+}
+
+function showBackToTopButtonOnScroll(){
+    if(scrollY > 450){
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
 }
 
 function openMenu(){
@@ -14,3 +29,21 @@ function openMenu(){
 function closeMenu(){
     document.body.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+}).reveal(`#home, 
+ #home img, 
+ #home .stats, 
+ #services,
+ #services header,
+ #services .card,
+ #about,
+ #about header,
+ #about content`);
+
+ function show(){
+
+ }
