98f4d85454fe36493fc394456e9b293baddd76b9
Gabriella Oliveira <gaby999@outlook.com>
2022-05-09 16:22:02 -0300

    stage-5-js

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # NLW Return - Trilha Origin
 
+####[Versão live do projeto](https://gaabyaolivera.github.io/nlw-origin/)
+####[Versão live do projeto modificado]()
+O evento aconteceu durante os dias 01/05 - 08/05, criamos uma aplicação do zero, a trilha Origin foi comandada pelo instrutor Mayky Brito, que possui uma excelente didática, obtive conhecimentos que vão agregar muito na minha carreira como desenvolvedora e me sinto mais confiante para me aventurar em novos projetos.
+
+- Empresa: [Rocketseat](https://www.rocketseat.com.br/sobre)
 - Link do projeto no [Figma](https://www.figma.com/community/file/1102912263666619803/DoctorCare)
-- Tecnologias utilizadas: HTML + CSS + JS
+
+
+Tecnologias utilizadas
+1. HTML
+2. CSS
+3. JS
+4. API - ScrollReveal
--- main.js
+++ main.js
@@ -4,8 +4,36 @@ onScroll()
 function onScroll() {
     showNavOnScroll()
     showBackToTopButtonOnScroll()
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(contact)
 }
+function activateMenuAtCurrentSection(section){
+    const targetLine = scrollY + innerHeight / 2;
 
+    //passou da linha?
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionTopReachorPassedTargetLine = targetLine >= sectionTop
+    
+
+    //está abaixo da linha alvo?
+    const sectionEndsAt = sectionTop + sectionHeight;
+    const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+
+    // boundaries
+    const sectionBoundaries = !sectionEndPassedTargetline && sectionTopReachorPassedTargetLine;
+
+    const sectionId = section.getAttribute('id')
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+    menuElement.classList.remove('active')
+    if(sectionBoundaries){
+        menuElement.classList.add('active')
+    }
+}
 function showNavOnScroll(){
     if(scrollY > 0){
         navigation.classList.add('scroll')
